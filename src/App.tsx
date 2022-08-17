68bc0b1d54a17ddd1e733c5c06e5c41f85001b66
adrian <adrianj1903@gmail.com>
2022-08-17 19:52:35 +0300

    Starting Home screen & Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,26 @@
+import { LocalDining } from '@mui/icons-material';
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import useFetchClothingItems from './hooks/useFetchClothingItems/useFetchClothingItems';
+import Home from './screens/Home/Home.index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
+import NavBar from './components/AppBar/AppBar.index';
 
 function App() {
+  const { loading } = useFetchClothingItems();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <NavBar />
+      {loading ? <CircularProgress /> :
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="users/*" element={<Users />} /> */}
+          </Routes>
+        </BrowserRouter>
+      }
     </div>
   );
 }
---
+++ src/screens/Home/Home.index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface Props {
+}
+
+const Home: React.FC = (props: Props) => {
+    return <p>null</p>;
+};
+
+export default Home;
