ec7fd07b036d7e69ebb8d60fc86ba050fbbba86d
adrian <adrianj1903@gmail.com>
2022-08-17 20:06:23 +0300

    Continue routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import { LocalDining } from '@mui/icons-material';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import useFetchClothingItems from './hooks/useFetchClothingItems/useFetchClothingItems';
 import Home from './screens/Home/Home.index';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 import NavBar from './components/AppBar/AppBar.index';
+import SavedOutfits from './screens/Home/SavedOutfits/SavedOutfits.index';
+
+enum Screens {
+  Home = "מסך הבית",
+  SavedOutfits = 'מסך פריטי לבוש'
+}
+
+const renderCurrScreen = (currScreen: Screens) => {
+  if (currScreen === Screens.Home) {
+    return <Home />
+  }
+
+  return <SavedOutfits />
+}
+
 
 function App() {
   const { loading } = useFetchClothingItems();
+  const [currScreen, setCurrScreen] = useState(Screens.SavedOutfits);
 
   return (
     <div className="App">
-      <NavBar />
+      <NavBar currScreen={currScreen} />
       {loading ? <CircularProgress /> :
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="users/*" element={<Users />} /> */}
-          </Routes>
-        </BrowserRouter>
+        renderCurrScreen(currScreen)
       }
-    </div>
+    </div >
   );
 }
 
